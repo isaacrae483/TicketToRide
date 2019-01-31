b3a588f4f3fd72c108ce40ebc2e4ebcf0a9f9c74
Kendall Despain <kdwspain@gmail.com>
2019-01-31 14:33:15 -0700

    Rough interfaces for Login and Game list views

--- app/src/main/java/com/runninglight/tickettoride/Presenter/View/Interfaces/IGameList_Presenter.java
+++ app/src/main/java/com/runninglight/tickettoride/Presenter/View/Interfaces/IGameList_Presenter.java
@@ -3,9 +3,9 @@ package com.runninglight.tickettoride.Presenter.View.Interfaces;
 public class IGameList_Presenter {
     IGameList_Presenter(){}
 
-    void createGame(){}
-    void joinGame(){}
+    private void createGame(){}
+    private void joinGame(){}
 
-    void updateGameListView(){}
+    private void updateGameListView(){}
 
 }
--- app/src/main/java/com/runninglight/tickettoride/Presenter/View/Interfaces/ILogin_Presenter.java
+++ app/src/main/java/com/runninglight/tickettoride/Presenter/View/Interfaces/ILogin_Presenter.java
@@ -1,4 +1,9 @@
 package com.runninglight.tickettoride.Presenter.View.Interfaces;
 
 public class ILogin_Presenter {
+
+    private void login(){}
+    private void register(){}
+
+    private void loginSuccess(){}
 }
