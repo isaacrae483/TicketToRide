e7ac2cd5f5ff0e40e37eb14c92ee97d0e6d5fb4b
Kendall Despain <kdwspain@gmail.com>
2019-01-31 14:36:23 -0700

    Adding interface for createGame

---
+++ app/src/main/java/com/runninglight/tickettoride/Presenter/View/Interfaces/ICreateGame_Presenter.java
@@ -0,0 +1,9 @@
+package com.runninglight.tickettoride.Presenter.View.Interfaces;
+
+public class ICreateGame_Presenter {
+    private void createGame(){}
+    private void joinGame(){}
+    private void cancel(){}
+
+    private void gameCreated(){}
+}
