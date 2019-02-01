6db2d2e1c883d247d1fd566647ea56918533c6ee
Kendall Despain <kdwspain@gmail.com>
2019-02-01 12:51:19 -0700

    some new packages and working on loginFragment

---
+++ app/src/main/java/com/runninglight/tickettoride/activity/LoginFragment.java
@@ -0,0 +1,7 @@
+package com.runninglight.tickettoride.activity;
+
+import android.support.v4.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+}
--- app/src/main/java/com/runninglight/tickettoride/activity/MainActivity.java
+++ app/src/main/java/com/runninglight/tickettoride/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.runninglight.tickettoride;
+package com.runninglight.tickettoride.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.runninglight.tickettoride.R;
 
 public class MainActivity extends AppCompatActivity
 {
