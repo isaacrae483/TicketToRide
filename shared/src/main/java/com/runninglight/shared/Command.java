be3b0d04abe3a9db7a418964502283e9edfa4a98
crmcclellan <36394369+crmcclellan@users.noreply.github.com>
2019-02-01 10:42:25 -0700

    Added javadocs including pre/post

--- shared/src/main/java/com/runninglight/shared/Command.java
+++ shared/src/main/java/com/runninglight/shared/Command.java
@@ -16,6 +16,17 @@ public class Command {
     private Object[] paramValues;
     private static Gson gson = new Gson();
 
+    /**
+     * Command constructor
+     *
+     * @param className name of the class that will have a method invoked
+     * @param methodName name of method to invoke
+     * @param paramTypeNames collection of names of the parameters' types
+     * @param paramValues collection of parameters
+     *
+     * @pre className, methodName, paramTypeNames, paramValues != null
+     * @post A new Command object is initialized with the given parameters
+     */
     public Command(String className, String methodName,
                    String[] paramTypeNames, Object[] paramValues) {
         this.className = className;
@@ -28,6 +39,14 @@ public class Command {
         }
     }
 
+    /**
+     * Server-side constructor that uses an InputStreamReader
+     *
+     * @param inputStreamReader the InputStreamReader that should contain the
+     *                          JSON information necessary to create a Command object
+     * @pre inputStreamReader != null and inputStreamReader contains JSON information for a Command
+     * @post A Command object is initialized by drawing information from the InputStreamReader
+     */
     public Command(InputStreamReader inputStreamReader) {
         Command tempCommand = (Command) gson.fromJson(inputStreamReader, Command.class);
         className = tempCommand.getClassName();
@@ -41,7 +60,13 @@ public class Command {
         }
     }
 
-    private final void createParameterTypes() {
+    /**
+     * Populates paramTypes by searching for the class names in paramTypeNames
+     *
+     * @pre paramTypeNames != null
+     * @post Fills paramTypes with the appropriate classes named in paramTypeNames
+     */
+    private void createParameterTypes() {
         paramTypes = new Class<?>[paramTypeNames.length];
         for(int i = 0; i < paramTypeNames.length; i++) {
             try {
@@ -54,6 +79,14 @@ public class Command {
         }
     }
 
+    /**
+     * Executes a method in a specific class
+     *
+     * @return The object created from the specified class's method or an exception
+     *
+     * @pre className, methodName, paramTypes, paramValues != null
+     * @post The method named methodName is invoked from the class named className if it exists
+     */
     public Object execute() {
         try {
             Class<?> receiver = Class.forName(className);
@@ -67,6 +100,16 @@ public class Command {
         }
     }
 
+    /**
+     * Gets a class with the specified name
+     *
+     * @param className name of class to search for
+     * @return A class with the specified name
+     * @throws ClassNotFoundException if the class does not exist
+     *
+     * @pre className != null
+     * @post returns the class with the specified name
+     */
     private static Class<?> getClassFor(String className)
             throws ClassNotFoundException
     {
