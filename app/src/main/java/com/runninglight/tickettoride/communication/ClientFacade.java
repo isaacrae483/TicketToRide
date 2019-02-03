45f7ecbeffe1b1847e0bbd237b904a63bd798d99
djj1229 <devin.j.johnson7@gmail.com>
2019-02-02 21:38:54 -0700

    Skeleton classes
    Just an initial commit with the classes and basic methods/data members

---
+++ app/src/main/java/com/runninglight/tickettoride/communication/ClientFacade.java
@@ -0,0 +1,17 @@
+package com.runninglight.tickettoride.communication;
+
+import com.runninglight.shared.Game;
+import com.runninglight.shared.IClient;
+import com.runninglight.shared.User;
+
+public class ClientFacade implements IClient {
+    @Override
+    public void addPlayer(User u) {
+        
+    }
+
+    @Override
+    public void addGame(Game g) {
+
+    }
+}
---
+++ shared/src/main/java/com/runninglight/shared/IClient.java
@@ -0,0 +1,8 @@
+package com.runninglight.shared;
+
+public interface IClient {
+
+    void addPlayer(User u);
+
+    void addGame(Game g);
+}
