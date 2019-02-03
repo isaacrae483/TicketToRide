7832577afc4f05b80a2b9994b8e7366a1125cdd0
Kendall Despain <kdwspain@gmail.com>
2019-02-02 21:39:34 -0700

    added some strings, created presenters, fixed the interfaces

---
+++ app/src/main/java/com/runninglight/tickettoride/presenter/CreateGame_Presenter.java
@@ -0,0 +1,34 @@
+package com.runninglight.tickettoride.presenter;
+
+import com.runninglight.shared.GameInfo;
+import com.runninglight.tickettoride.IPresenter.ICreateGame_Presenter;
+
+import java.util.Observable;
+
+public class CreateGame_Presenter implements ICreateGame_Presenter {
+
+    @Override
+    public void createGame(GameInfo gameInfo) {
+
+    }
+
+    @Override
+    public void joinGame(GameInfo gameInfo) {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public void gameCreated() {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
---
+++ app/src/main/java/com/runninglight/tickettoride/presenter/GameLobby_Presenter.java
@@ -0,0 +1,17 @@
+package com.runninglight.tickettoride.presenter;
+
+import com.runninglight.tickettoride.IPresenter.IGameLobby_Presenter;
+
+import java.util.Observable;
+
+public class GameLobby_Presenter implements IGameLobby_Presenter {
+    @Override
+    public void StartGame() {
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
