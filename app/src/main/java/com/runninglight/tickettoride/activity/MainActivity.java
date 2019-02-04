74c9e5d3ff973bf885089c91dfaa21fc19e9648e
Kendall Despain <kdwspain@gmail.com>
2019-02-04 12:09:59 -0700

    my stuff

--- app/src/main/java/com/runninglight/tickettoride/activity/MainActivity.java
+++ app/src/main/java/com/runninglight/tickettoride/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.runninglight.tickettoride.activity;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,10 +9,25 @@ import com.runninglight.tickettoride.R;
 public class MainActivity extends AppCompatActivity
 {
 
+    private LoginFragment loginFragment;
+    private GameListFragment gameListFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = this.getSupportFragmentManager();
+
+        loginFragment = (LoginFragment) fm.findFragmentById(R.id.currentFragment_View);
+        if(loginFragment==null){
+            loginFragment= new LoginFragment();
+            fm.beginTransaction()
+                    .add(R.id.currentFragment_View,loginFragment)
+                    .commit();
+        }
+
+
     }
 }
