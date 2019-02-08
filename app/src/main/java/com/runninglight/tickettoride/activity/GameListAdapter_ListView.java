d9f71841acd1167bc9231701c3da90db29b6420b
Kendall Despain <kdwspain@gmail.com>
2019-02-08 11:35:59 -0700

    making a list view

---
+++ app/src/main/java/com/runninglight/tickettoride/activity/GameListAdapter_ListView.java
@@ -0,0 +1,54 @@
+package com.runninglight.tickettoride.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.runninglight.shared.Game;
+import com.runninglight.tickettoride.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.runninglight.tickettoride.R.layout.recyclerview_row_gamelist;
+
+
+public class GameListAdapter_ListView extends ArrayAdapter<Game>{
+
+    private final Activity context;
+    private final Game[] games;
+
+
+    public GameListAdapter_ListView (Activity context,Game[] myGames){
+
+        super(context, recyclerview_row_gamelist , myGames);
+
+        this.context = context;
+        games =myGames ;
+
+    }
+
+
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(recyclerview_row_gamelist, null, true);
+
+        TextView gameName = rowView.findViewById(R.id.gameName_textView_recyclerRow);
+        TextView currentPlayers = rowView.findViewById(R.id.currentPlayerNum_textView_recyclerRow);
+
+        gameName.setText(games[position].getGameName());
+        currentPlayers.setText(games[position].getNumPlayers() + "/" + games[position].getMaxPlayerNumber());
+
+
+        return rowView;
+    }
+
+    }
---
+++ app/src/main/java/com/runninglight/tickettoride/activity/GameListFragment_ListView.java
@@ -0,0 +1,120 @@
+package com.runninglight.tickettoride.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.runninglight.shared.Game;
+import com.runninglight.shared.GameInfo;
+import com.runninglight.tickettoride.IPresenter.IGameList_Presenter;
+import com.runninglight.tickettoride.R;
+import com.runninglight.tickettoride.communication.ClientModel;
+import com.runninglight.tickettoride.iview.IGameList_View;
+import com.runninglight.tickettoride.presenter.GameList_Presenter;
+
+public class GameListFragment_ListView extends Fragment implements IGameList_View
+{
+
+    private GameListAdapter_ListView adapter;
+    private IGameList_Presenter presenter;
+
+    public GameListFragment_ListView()
+    {
+
+        presenter = new GameList_Presenter(this);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Game[] games;
+        games = ClientModel.getInstance().getGameList().toArray(new Game[0]);
+        if(games == null){
+            System.out.println("games was null");
+            games = new Game[0];
+        }
+        //ClientModel.getInstance().getGameList().size()
+        adapter = new GameListAdapter_ListView(getActivity(), games);
+
+        startRefresher();
+    }
+
+    private void startRefresher(){
+        final Handler handler = new Handler();
+        final int delay = 5000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                adapter.notifyDataSetChanged();
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_gamelist_listview, container, false);
+
+        final ListView gameList_LV = v.findViewById(R.id.gameList_listView);
+
+        Button create_BTN = v.findViewById(R.id.create_button_gameList_listView);
+
+        //TODO: pass game list in from the model
+        //how to pass the game list down from the model without storing it?
+
+
+        gameList_LV.setAdapter(adapter);
+        gameList_LV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = gameList_LV.getItemAtPosition(position);
+                Game game = (Game)o;
+                presenter.joinGame(new GameInfo(game.getGameName(),game.getMaxPlayerNumber()));
+            }
+        });
+
+
+
+        create_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.createGame();
+            }
+        });
+
+
+        return v;
+    }
+
+    @Override
+    public void createGame() {
+        Intent intent = new Intent(getContext(),CreateGameActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void joinGameSuccessful(GameInfo gameInfo) {
+
+        Intent intent = new Intent(getContext(),GameLobbyActivity.class);
+
+        //TODO: add game info to the intent bundle
+        startActivity(intent);
+
+    }
+}
