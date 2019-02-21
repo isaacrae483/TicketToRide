077960b6cf4a3897598a84362ef9a1d461bfaf53
despaink <43147974+despaink@users.noreply.github.com>
2019-02-20 20:31:07 -0700

    added a comment to help people do their things

--- app/src/main/java/com/runninglight/tickettoride/activity/game/GameActivity.java
+++ app/src/main/java/com/runninglight/tickettoride/activity/game/GameActivity.java
@@ -2,6 +2,7 @@ package com.runninglight.tickettoride.activity.game;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import com.runninglight.tickettoride.IPresenter.game.IGameActivity_Presenter;
 import com.runninglight.tickettoride.iview.game.IGameActivity_View;
@@ -11,10 +12,19 @@ public class GameActivity extends AppCompatActivity implements IGameActivity_Vie
 
     private IGameActivity_Presenter presenter;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         presenter = new GameActivity_Presenter(this);
+
+        //TODO: see MainActivity for example of how to put fragments into frames
+
+        //TODO: the other option is to change the frames to literal fragments once all the
+        //TODO: fragments are done
+
 
     }
 }
