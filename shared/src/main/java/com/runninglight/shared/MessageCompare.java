43707742ed51d8c11c42955cf3961a0075cf79fe
Tristan <twthender@gmail.com>
2019-02-25 12:29:14 -0700

    Update MessageCompare.java
    got rid of debug statements

--- shared/src/main/java/com/runninglight/shared/MessageCompare.java
+++ shared/src/main/java/com/runninglight/shared/MessageCompare.java
@@ -7,9 +7,6 @@ public class MessageCompare implements Comparator<Message>
     @Override
     public int compare(Message message1, Message message2)
     {
-        System.out.println("comparing: " + (int) (message1.getTimestamp() - message2.getTimestamp()));
-        System.out.println(message1.getTimestamp());
-        System.out.println(message2.getTimestamp());
         return (int) (message1.getTimestamp() - message2.getTimestamp());
     }
 }
