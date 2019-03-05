d47dece525a6108b6f067b4a2754e05e356f4560
despaink <43147974+despaink@users.noreply.github.com>
2019-03-04 20:14:20 -0700

    made a circle button

--- app/src/main/java/com/runninglight/tickettoride/activity/game/MapFragment.java
+++ app/src/main/java/com/runninglight/tickettoride/activity/game/MapFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.ImageViewCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.runninglight.tickettoride.R;
 
 public class MapFragment extends Fragment {
+
+    private ImageView map;
 
     public MapFragment(){}
 
@@ -26,6 +32,18 @@ public class MapFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        map = v.findViewById(R.id.map_image_IV);
+
+        map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()== MotionEvent.ACTION_UP){
+                    Toast.makeText(getActivity(),(int) event.getX(),Toast.LENGTH_LONG);
+                }
+                return false;
+            }
+        });
 
         return v;
     }
